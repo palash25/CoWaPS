df2bdc057cd111c4de956db73c079da7c2831e3b
Swaraj Hota <swarajhota353@gmail.com>
2017-12-12 16:17:54 +0530

    Fixed open issues #1 and #2

--- scrape.py
+++ scrape.py
@@ -2,26 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def url_builder():
+    uname = raw_input("Enter user name: ")
+    url = 'https://www.codewars.com/users/' + uname
+    return url
+
+
 def fetch(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
-    user_info = soup.find_all('div', attrs={'class' : 'stat'})
+    user_info = soup.find_all('div', attrs={'class': 'stat'})
     counter = 0
 
     for info in user_info:
         counter += 1
 
-        if(counter>=5 and counter<=7):
+        if(counter >= 5 and counter <= 7):
             continue
 
-        print(info.contents[0].string," ",info.contents[1].string)
+        print(info.contents[0].string, " ", info.contents[1].string)
 
-        if(counter==16):
+        if(counter == 16):
             break
 
 
 def main():
-    fetch('https://www.codewars.com/users/fenster25')
+    fetch(url_builder())
 
 
 if __name__ == '__main__':
